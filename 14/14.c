1a60408e8551f534ccaf26a93ef2a11b3c07e48e
huangqianhong <1510851768@qq.com>
2018-05-07 09:38:16 +0800

    Add files via upload

--- 14/14.c
+++ 14/14.c
@@ -1,1 +1,37 @@
+//从小到大排序
+#include<stdio.h>
+void sort(int a,int b,int c)
+{
+	int t;
+	if(a>b)             //a和b已经排好序了
+	{
+		t=a;
+		a=b;
+		b=t;
+	}
+	
+	if(a>c)
+	{
+		t=a;
+		a=c;
+		c=t;
+	}
 
+	if(b>c)                 
+	{
+		t=b;
+		b=c;
+		c=t;
+	}
+	printf("%d\t,%d\t,%d\t",a,b,c);
+
+
+}
+void main()
+{
+	int a,b,c;
+	printf("请输入三个整数:");
+	scanf("%d %d %d",&a,&b,&c);
+	sort(a,b,c);
+
+}
