d9343aca25dcd9f582ff9b84151a07168547a156
huangqianhong <1510851768@qq.com>
2018-05-07 22:16:53 +0800

    Add files via upload

--- 2/2.c
+++ 2/2.c
@@ -1,1 +1,37 @@
+//统计字符串的大写字母，小写字母，数字出现的个数
+#include<stdio.h>
+void f()         //bchar代表大写字母,bcahr代表小写字母的个数
+{
+	int c, digita,bchar,schar;
+	digita=0;
+	bchar=0;
+	schar=0;
 
+	
+	while((c=getchar())!='\n')                  //接受一个字符，直到'\n'统计结束
+	{
+		if(c>='0'&&c<='9')
+		{
+			digita+=1;
+		}
+		if(c>='a'&&c<'z')
+		{
+			schar+=1;
+		}
+		if(c>='A'&&c<='Z')
+		{
+			bchar+=1;
+		}
+		
+	}
+	
+	printf("大写字母个数为:%d\n",bchar);
+	printf("小写字母个数为:%d\n",schar);
+	
+	printf("数字个数为:%d\n",digita);
+	
+}
+void main()
+{
+	f();
+}
