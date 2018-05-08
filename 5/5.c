47a1004670c301d47693649b1669dab7cab6ac9b
huangqianhong <1510851768@qq.com>
2018-05-08 09:30:05 +0800

    Add files via upload

--- 5/5.c
+++ 5/5.c
@@ -1,1 +1,17 @@
+#include<stdio.h>
+void main()
+{
+	int n,i;           //n代表对折的次数
+	int p,x=1;             //p代表面条的根数
+	printf("请输入面条的对折的次数:");
+	scanf("%d",&n);
+	for(i=0;i<n+1;i++)
+	{
+		p=x+1;
+		x=2*x;
+	
+	}
+	printf("%d",p);
 
+
+}
