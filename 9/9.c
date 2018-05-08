3494ae32fe0495ca0452c39c670ee3cbf507810b
huangqianhong <1510851768@qq.com>
2018-05-08 22:40:48 +0800

    Add files via upload

--- 9/9.c
+++ 9/9.c
@@ -1,1 +1,40 @@
+/*．给定N个正整数，请统计奇数和偶数各有多少个*/
+#include<stdio.h>
+void f()
+{
+	int i;
+	int a=0;
+	int b=0;
+	int num[256]={0};
+	int n;
+	scanf("%d",&n);
+	if(n<1000)
+	{
+		for(i=0;i<n;i++)
+		{
+			scanf("%d",&num[i]);
+		}
+		printf("\n");
+		for(i=0;i<n;i++)
+		{
+			if(num[i]%2!=0)
+			{
+			  a++;
+			}
+		    else
+			{
+			  b++;
+			}
+		}		
+	}
+	printf("%d %d",a,b);
 
+}
+void main()
+{
+	
+	f();
+	
+
+
+}
