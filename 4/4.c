c3d91e8a480d7b7e29739cfb8922a5db238eb9b8
huangqianhong <1510851768@qq.com>
2018-05-08 08:59:34 +0800

    Add files via upload

--- 4/4.c
+++ 4/4.c
@@ -1,1 +1,25 @@
+#include<stdio.h>
+void f(int p)            //x,y,z分别代表百位、十位、个位
+{
+	
+	int x,y,z;
+	x= p%10;
+	y=p/10%10;
+	z=p/100%10;
+	if(((p*p)%10==x)&&((p*p)/10%10==y)&&((p*p)/100%10==z))
+	{
+		printf("%ld\n",p);
+	}
 
+	
+
+}
+void main()
+{
+	int i;
+	for(i=100;i<1000;i++)
+	{
+		f(i);
+
+	}
+}
