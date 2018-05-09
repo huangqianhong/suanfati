17012152b841e3ec2403f2d14e85fb40070aa774
huangqianhong <1510851768@qq.com>
2018-05-09 11:13:35 +0800

    Add files via upload

--- 10/10.c
+++ 10/10.c
@@ -1,1 +1,27 @@
+/*将大写字母变成小写字母*/
+#include<stdio.h>
+#include<stdlib.h>
+void f()
+{
+	char a[21]={0};
+	char c;
+	int i=1;
+	while((c=getchar())!='\n'&&i<21)            //输入字符直到空格为止，然后一个一个接收
+	{
+		if('a'<=c&&c<='z')
+		{
+			printf("%c",c-32);
+		}
+		if('A'<=c&&c<='Z')
+		{
+			printf("%c",c+32);
+		}
+		i++;
+	}
+    printf("\n");
 
+}
+void main()
+{
+	f();
+}
