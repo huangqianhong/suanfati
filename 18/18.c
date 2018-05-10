41389edc2187a3f17de9b365a8361ce5c274b656
huangqianhong <1510851768@qq.com>
2018-05-10 21:12:32 +0800

    Add files via upload

--- 18/18.c
+++ 18/18.c
@@ -1,1 +1,25 @@
+/*汉诺塔问题*/
+#include<stdio.h>
+int count;
+void f(int n,char a,char b,char c)
+{
+	if(n==1)
+	{
+		printf("第%d次:%c->%c\n",++count,a,c);
+	}
+	else
+	{
+		f(n-1,a,c,b);
+		printf("第%d次:%c->%c\n",++count,a,c);
+		f(n-1,b,a,c);
+	}
 
+}
+void main()
+{
+	int n;
+	scanf("%d",&n);
+	count=0;
+	f(n,'a','b','c');
+
+}
