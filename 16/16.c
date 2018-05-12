32f4f81dd51bedb976f35fc8e2e9912fb175d1ea
huangqianhong <1510851768@qq.com>
2018-05-12 17:31:40 +0800

    Add files via upload

--- 16/16.c
+++ 16/16.c
@@ -1,1 +1,26 @@
+//´òÓ¡×Ö·û´®
+#include<stdio.h>
+#include<string.h>
+#define maxsize 20
+void main()
+{
+	int i,len;
+	char str;
+	char num[maxsize];
+	gets(num);
+	len=strlen(num);
+	scanf("%c",&str);
+	for(i=0;i<len;i++)
+	{
+	    while(num[i]==str)
+		{
+			i++;
+		}
+		if(i==len)
+		{
+			printf("Ã»ÓÐ×Ö·û´®!");
+		}
+		printf("%c",num[i]);
 
+	}
+}
